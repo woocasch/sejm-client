0902b246483edf829887eec26a2bcb7330015707
Łukasz Nowakowski <lukasz@lnow.pl>
2025-04-28 22:13:53 +0200

    Add call to retrieve members.

--- front/src/apis/sejm-api-client.ts
+++ front/src/apis/sejm-api-client.ts
@@ -1,4 +1,4 @@
-export interface GetTermsRequest {}
+export interface GetTermsRequest { }
 
 export interface TermPrintsData {
   count?: number;
@@ -18,18 +18,62 @@ export interface GetTermsResponse {
   terms?: TermInfo[];
 }
 
+export interface GetParliamentMembersRequest {
+  termId: number;
+}
+
+export interface ParliamentMember {
+  accusativeName: string;
+  active: boolean;
+  birthDate: string;
+  birthLocation: string;
+  club: string;
+  districtName: string;
+  districtNum: number;
+  educationLevel: string;
+  email: string;
+  firstLastName: string;
+  firstName: string;
+  genitiveName: string;
+  id: number;
+  lastFirstName: string;
+  lastName: string;
+  numberOfVotes: number;
+  profession: string;
+  secondName: string;
+  voivodeship: string;
+}
+
+export interface GetParliamentMembersResponse {
+  members: ParliamentMember[];
+}
+
 export interface ISejmApiClient {
   GetTerms(request: GetTermsRequest): Promise<GetTermsResponse>;
+
+  GetParliamentMembers(request: GetParliamentMembersRequest): Promise<GetParliamentMembersResponse>;
 }
 
 export class SejmApiClient implements ISejmApiClient {
+  private readonly rootAddress: string = 'https://api.sejm.gov.pl/sejm/term'
+
   public async GetTerms(request: GetTermsRequest): Promise<GetTermsResponse> {
-    const response = await fetch('https://api.sejm.gov.pl/sejm/term');
+    const response = await fetch(this.rootAddress);
     const json = await response.json();
     const terms = <TermInfo[]>json;
     return {
       terms: terms,
     };
+  }
+
+  public async GetParliamentMembers(request: GetParliamentMembersRequest): Promise<GetParliamentMembersResponse> {
+    const address = `${this.rootAddress}${request.termId}/MP`;
+    const response = await fetch(address);
+    const json = await response.json();
+    const members = <ParliamentMember[]>json;
+    return {
+      members: members,
+    }
   }
 }
 
