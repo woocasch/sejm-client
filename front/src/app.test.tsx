97d7b751340f000da1d71be539587e0fcc529720
Łukasz Nowakowski <lukasz@lnow.pl>
2025-04-28 10:48:11 +0200

    Fix test
    When putting navigation in component, router is needed for it to render correctly.

--- front/src/app.test.tsx
+++ front/src/app.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import AppComponent from './app';
+import { BrowserRouter } from 'react-router';
 
 describe('AppComponent', () => {
   it('should be rendered', () => {
-    render(<AppComponent />);
+    render(
+      <BrowserRouter>
+        <AppComponent />
+      </BrowserRouter>,
+    );
   });
 });
